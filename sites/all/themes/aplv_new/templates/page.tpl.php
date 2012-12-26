41f8db42dd10e41aaaabafa2f93e5598f6cbee2d
Thomas Moseler <tmoseler@gmx.de>
2012-12-26 14:36:08 +0100

    Heavy re-architecting of the theme. Added coloring to footer, moved title into title bar, changed color of headings. All this is not final, but in progress.

--- sites/all/themes/aplv_new/templates/page.tpl.php
+++ sites/all/themes/aplv_new/templates/page.tpl.php
@@ -73,7 +73,16 @@
  */
 ?>
 
-  <div id="page-wrapper"><div id="page">
+  <div id="page-wrapper">
+      <div id="title-wrapper">
+        <div id="title-wrapper-inner">
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+            <?php print render($title_suffix); ?>
+          </div>
+    </div>
+
+    <div id="page">
 
     <div id="header"><div class="section clearfix">
 
@@ -130,9 +139,6 @@
       <div id="content" class="column"><div class="section">
         <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
         <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-        <?php print render($title_suffix); ?>
         <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
         <?php print render($page['help']); ?>
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
