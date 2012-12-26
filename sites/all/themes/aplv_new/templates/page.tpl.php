3e6f152ab26b01e20b4d360acbdee7dac4083dc6
Thomas Moseler <tmoseler@gmx.de>
2012-12-26 19:31:21 +0100

    More styling for all kinds of stuff

--- sites/all/themes/aplv_new/templates/page.tpl.php
+++ sites/all/themes/aplv_new/templates/page.tpl.php
@@ -79,6 +79,12 @@
             <?php print render($title_prefix); ?>
             <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
             <?php print render($title_suffix); ?>
+
+            <?php if ($page['flags']): ?>
+              <div id="flags" class="clearfix"><div class="section">
+                <?php print render($page['flags']); ?>
+              </div></div> <!-- /.section, /#flags -->
+            <?php endif; ?>
           </div>
     </div>
 
